5acacb860acc0cf7f7554d2062980e99a4239468
ATonom <atonomrus@gmail.com>
2020-11-29 21:51:48 +0500

    [PAS][Vinny] Добавлена функция СalculationDistans
    Расчет растояния между двумя точками.

--- Viny/PAS/Vinny.pas
+++ Viny/PAS/Vinny.pas
@@ -8,10 +8,18 @@ begin
 	Readln(integerVar);
 	ReadInteger := integerVar
 end;
+
+Function СalculationDistans(pointOneX,pointOneY,pointTwoX,pointTwoY :integer) :real;
+begin
+	СalculationDistans := sqrt((pointTwoX - pointOneX) * (pointTwoX - pointOneX) + (pointTwoY - pointOneY) * (pointTwoY - pointOneY))
+end;
+
+
 var
 	viHomeX, viHomeY :integer;
 	pyHomeX, pyHomeY :integer;
 	ravineX, ravineY, ravineRadius :Integer;
+	viHomeRavineDistance, pyHomeRavineDistance :real;
 begin 
 	viHomeX := ReadInteger('Введи координату X дома Винни:');
 	viHomeY := ReadInteger('Введи координату Y дома Винни:');
@@ -21,4 +29,9 @@ begin
 	ravineY := ReadInteger('Введи координату Y Оврага:');
 	ravineRadius := ReadInteger('Введи радиус Оврага:');
 
+	viHomeRavineDistance := СalculationDistans(viHomeX,viHomeY,ravineX,ravineY);
+	writeln('Растояние от дома Винни до центра оврага:', viHomeRavineDistance);
+
+	pyHomeRavineDistance := СalculationDistans(viHomeX,viHomeY,ravineX,ravineY);
+	writeln('Растояние от дома Пяточка до центра оврага:', pyHomeRavineDistance);
 end.

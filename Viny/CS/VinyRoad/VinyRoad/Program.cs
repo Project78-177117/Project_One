10303bbe06cf89c4118e7992803b35b4ab148a1f
Project177117 <Project177117@gmail.com>
2020-11-29 14:36:05 +0500

    First iteration
    Create Sum Method and Read Method

--- Viny/CS/VinyRoad/VinyRoad/Program.cs
+++ Viny/CS/VinyRoad/VinyRoad/Program.cs
@@ -8,8 +8,33 @@ namespace VinyRoad
 {
     class Program
     {
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Read() 
+        {
+            int c;
+            c = int.Parse(Console.ReadLine());
+
+            return c;
+        }
+
         static void Main(string[] args)
         {
+            int x, y, z;
+
+            Console.WriteLine("Введите первое слогаемое");
+            x = Program.Read();
+
+            Console.WriteLine("Введите второе слогаемое");
+            y = Program.Read();
+
+            z = Sum(x, y);
+
+            Console.WriteLine("Сумма " + x + " и " + y + " равна - " + z +"!");
+            Console.ReadLine();
         }
     }
 }

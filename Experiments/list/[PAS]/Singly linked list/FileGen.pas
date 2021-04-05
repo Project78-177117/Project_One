bef8416182d82bb8bd559f83cdcf9d989d8c07ce
ATonom <atonomrus@gmail.com>
2021-04-05 22:05:49 +0500

    Новая папка Experiments и прочее (смотри комментарий)
    - в gidignore добавлены файлы с расширением .bin
    - Experiments\list\[PAS]\Singly linked list  (работа с односвязными списками)
    -  FileGen программа генерирует файл numbers.bin с заданным количеством чисел типа integer (программа готова)
    -  List_1 программа формирует динамический список чисел из файла  numbers.bin (программа в разработке)

---
+++ Experiments/list/[PAS]/Singly linked list/FileGen.pas
@@ -0,0 +1,26 @@
+﻿{Программа генерирует файл с заданным количеством чисел типа Intager.}
+program FileGen;
+
+const
+	fileName = 'numbers.bin';
+Var
+	list :file of integer;
+	numberX, amount, step :integer;
+
+
+begin
+	write('Введи количество чисел:');
+	readln(amount);
+
+	assign(list, fileName);
+	rewrite(list);
+	
+	for step := 1 to amount do
+	begin
+		numberX := random(200) - 100;
+		write(list, numberX);
+	end;
+
+	close(list);
+	write('В файл ', fileName, ' внесено ', amount, ' чисел от -100 до 100');
+end.
---
+++ Experiments/list/[PAS]/Singly linked list/List_1.pas
@@ -0,0 +1,38 @@
+﻿{Попытка сделать односвязный список
+Программа читает их файла числа из файла, а затем выводит их на экран}
+
+program List_1;
+
+type
+	itemPtr = ^item;
+	item = record
+		data: integer;
+		next, previous: itemPtr;
+	end;
+
+const
+	fileName = 'numbers.bin';
+
+var
+	number: itemPtr;
+	first, last: pointer;
+	list :file of integer;
+	x: integer;
+
+begin
+	assign(list, fileName);
+	reset(list);
+
+	while not Eof(list) do
+  	begin
+
+
+
+
+  		{Проверка}
+		{read(list,x);
+		writeln(x);}
+	end;
+
+	close(list);
+end.
